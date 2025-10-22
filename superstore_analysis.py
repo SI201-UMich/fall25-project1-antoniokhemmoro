3952d01e38b6d421c01fa80fdc83be83c618adf5
antoniokhemmoro <khemmoro@umich.edu>
2025-10-22 13:06:16 -0400

    First calcuation (calculate_avg_sales_by_category)

--- superstore_analysis.py
+++ superstore_analysis.py
@@ -1,4 +1,19 @@
 # Name: Antonio Khemmoro test
 # Student ID: 12345678
-# Email: 
-# Collaborators: Matthew Khemmoro
+# Email: khemmoro@umich.edu 
+
+def calculate_avg_sales_by_category(superstore_data):
+    category_totals = {}
+    category_counts = {}
+
+    for row in superstore_data:
+        category = row['Category']
+        sales = row['Sales']
+        category_totals[category] = category_totals.get(category, 0) + sales
+        category_counts[category] = category_counts.get(category, 0) + 1
+
+    avg_sales = {}
+    for cat in category_totals:
+        avg_sales[cat] = round(category_totals[cat] / category_counts[cat], 2)
+
+    return avg_sales
